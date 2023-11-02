6b5526051f13bc1a1d9377988e1acea0452dda15
IDHARDWELL <masimopostoluk2@gmail.com>
2023-11-02 11:53:47 +0200

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../FileName.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int n = 5;
+			int a[n] = { -5, -2, 0, 3, 4 };
+			int sum = Sum(a, n, n);
+			Assert::AreEqual(sum, 2);
+		}
+	};
+}
